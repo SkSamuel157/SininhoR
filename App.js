bddaabfd0aafc7894bd4490037da7cb3c2d95308
SkSamuel157 <samuelscs12senai@gmail.com>
2024-11-04 11:32:13 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import Pusher from 'pusher-js/react-native';
+
+// Credenciais do Pusher para o app "SininhoR"
+const pusher = new Pusher('dd78661dc83711748d87', {
+  cluster: 'us2'
+});
+
+const SininhoR = () => {
+  const [notificacoes, setNotificacoes] = useState([]);
+
+  useEffect(() => {
+    const channel = pusher.subscribe('my-channel');
+    channel.bind('my-event', (data) => {
+      setNotificacoes((prevNotificacoes) => [
+        ...prevNotificacoes,
+        data.message,
+      ]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, []);
+
+  return (
+    <View>
+      <Text>Notificações Recebidas:</Text>
+      <FlatList
+        data={notificacoes}
+        renderItem={({ item }) => <Text>{item}</Text>}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </View>
+  );
+};
+
+export default SininhoR;
